6236c6bb014bbb7849a66ec09608f2380e7585d4
deeepak-me <deepak.krishnan.e@gmail.com>
2025-11-14 16:11:12 +0530

    feat: getting start with flutter app

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//stateless
+//material app
+//scaffold
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Map"),
+          centerTitle: false,
+          leading: Icon(Icons.login),
+          actions: [Text("login"), Icon(Icons.login)],
+          backgroundColor: Colors.teal,
+        ),
+      ),
+    );
+  }
+}
